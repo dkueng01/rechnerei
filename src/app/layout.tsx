4058b89d45622527636ea4e76d9b5da17c82e2ca
dkueng <davidwichtig@gmx.at>
2025-11-14 22:48:26 +0100

    Auth setup changes (auth route not working at the moment)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+import ConvexClientProvider from "@/components/ConvexClientProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +27,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-      </body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,49 +8,49 @@ import {
   SidebarInset,
   SidebarProvider,
 } from "@/components/ui/sidebar"
-import { Authenticated, Unauthenticated, useQuery } from "convex/react"
-import { api } from "../../convex/_generated/api"
-import { useEffect } from "react"
+import { Authenticated, Unauthenticated, useConvexAuth } from "convex/react"
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  const user = useQuery(api.users.viewer);
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-  console.log("Current user:", user);
+  console.log("isAuthenticated:", isAuthenticated, "isLoading:", isLoading);
 
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 56)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <Authenticated>
-        <AppSidebar variant="inset" />
-        <SidebarInset>
-          <SiteHeader />
-          <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                <SectionCards />
-                <div className="px-4 lg:px-6">
-                  <ChartAreaInteractive />
+    <>
+    {/* <Authenticated> */}
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 56)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  <SectionCards />
+                  <div className="px-4 lg:px-6">
+                    <ChartAreaInteractive />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </SidebarInset>
-      </Authenticated>
+          </SidebarInset>
+        </SidebarProvider>
+      {/* </Authenticated>
       <Unauthenticated>
         <div className="flex h-screen w-full items-center justify-center">
           <p className="text-lg">LandingPage</p>
-          <a href="/login">
+          <a href="/signin">
             <Button className="ml-4">Log in</Button>
           </a>
         </div>
-      </Unauthenticated>
-    </SidebarProvider>
+      </Unauthenticated> */}
+    </>
   );
 }
